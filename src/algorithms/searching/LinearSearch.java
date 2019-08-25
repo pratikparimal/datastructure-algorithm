ce683a10475211befe385d3beaae85a791bf9dbb
pratikparimal <pratik.parimal@gmail.com>
2019-08-25 19:25:43 +0530

    added linear search

--- src/algorithms/searching/LinearSearch.java
+++ src/algorithms/searching/LinearSearch.java
@@ -1,4 +1,20 @@
 package algorithms.searching;
 
 public class LinearSearch {
+    public static void main(String[] args) {
+        int array[] = {3,9,6,1,0,8,2,7,6,6};
+        boolean search = linearSearch(array, 8);
+        System.out.println("Element present ? : " + search);
+    }
+
+    private static boolean linearSearch(int[] array, int key) {
+        boolean search = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == key) {
+                search = true;
+                break;
+            }
+        }
+        return search;
+    }
 }
